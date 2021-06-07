8f3857fd433bb7763e25a38136b6137ef577c500
Gary Geng <garygengxiao@gmail.com>
2021-06-07 17:12:00 -0500

    allow changing python interp and add help text

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,7 +1,7 @@
-import { privateEncrypt } from 'crypto';
+import { execPromise } from './utils';
 import * as path from 'path';
 import {
-	ExtensionContext, commands, SnippetString, window, Range, Position, DecorationOptions, workspace, Event, TextDocument, ColorThemeKind,
+	ExtensionContext, commands, SnippetString, window, Range, Position, DecorationOptions, workspace, Event, TextDocument, ColorThemeKind, Disposable, EnvironmentVariableMutatorType, languages, CodeLensProvider, CodeLens, Command, Uri,
 } from 'vscode';
 
 import {
@@ -12,13 +12,65 @@ import {
 } from 'vscode-languageclient/node';
 import { multiStepInput } from './annotationInput';
 
-let client: LanguageClient;
+let client: LanguageClient = null;
+let curPythonInterp: string | null = null;
+let lastChangeInterp = 0;
 
-export async function activate(context: ExtensionContext) {
-	// TODO allow the user to manually specify an interpreter to use, possibly leveraging the
-	// Python language extension. See https://code.visualstudio.com/api/references/vscode-api#extensions
-	// TODO this might be python3
-	const pythonInterp = 'python';
+
+class AntCodeLensProvider implements CodeLensProvider {
+	async provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
+		if (document.languageId === 'antimony' && !document.getText().trim()) {
+			const topOfDocument = new Range(0, 0, 0, 0);
+
+			let c: Command = {
+				title: 'BioIDE Help Page',
+				command: 'vscode.open',
+				arguments: [Uri.parse('https://github.com/evilnose/vscode-antimony#bioidek-vscode-extension')],
+			}
+			let codeLens = new CodeLens(topOfDocument, c)
+			return [codeLens];
+		}
+
+		return [];
+	}
+}
+
+// HACK returns 0 if fine, 1 if Python version is too low, or 2 if the process failed.
+async function verifyPythonInterp(path: string) {
+	try {
+		const result = await execPromise(`"${path}" -c "import sys; print(sys.version_info >= (3, 6))"`);
+		if (result['stdout'].trim() === 'True') {
+			return 0;
+		}
+		return 1;
+	} catch (e) {
+		return 2;
+	}
+}
+
+function getPythonInterpSetting(): string {
+	const config = workspace.getConfiguration('bio-ide');
+	return config.get('pythonInterpreter');
+}
+
+// start the language server client
+async function startLSClient(context: ExtensionContext) {
+	curPythonInterp = getPythonInterpSetting();
+
+	const error = await verifyPythonInterp(curPythonInterp);
+	if (error !== 0) {
+		let errMessage: string;
+		if (error === 1) {
+			errMessage = `Failed to launch language server: "${curPythonInterp}" is not Python 3.6+`;
+		} else {
+			errMessage = `Failed to launch language server: Unable to run "${curPythonInterp}"`;
+		}
+		const choice = await window.showErrorMessage(errMessage, 'Edit in settings');
+		if (choice === 'Edit in settings') {
+			await commands.executeCommand('workbench.action.openSettings', 'bio-ide.pythonInterpreter');
+		}
+		return;
+	}
 
 	const pythonMain = context.asAbsolutePath(
 		path.join('server', 'main.py')
@@ -27,7 +79,7 @@ export async function activate(context: ExtensionContext) {
 	const args = [pythonMain];
 
 	// Add debug options here if needed
-	const serverOptions: ServerOptions = { command: pythonInterp, args };
+	const serverOptions: ServerOptions = { command: curPythonInterp, args };
 
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [
@@ -43,18 +95,60 @@ export async function activate(context: ExtensionContext) {
 		clientOptions
 	);
 
+	// Start the client. This will also launch the server
+	const clientDisposable = client.start();
+	context.subscriptions.push(clientDisposable);
+}
+
+export async function activate(context: ExtensionContext) {
+	// TODO allow the user to manually specify an interpreter to use, possibly leveraging the
+	// Python language extension. See https://code.visualstudio.com/api/references/vscode-api#extensions
+	// TODO this might be python3
+	startLSClient(context);
+
 	context.subscriptions.push(
 		commands.registerCommand('antimony.createAnnotationDialog',
 			(...args: any[]) => createAnnotationDialog(context, args)));
 
-	// Start the client. This will also launch the server
-	client.start();
+	decorateDocument(window.activeTextEditor?.document);
+	workspace.onDidChangeConfiguration(async (e) => {
+		if (!e.affectsConfiguration('bio-ide')) {
+			return;
+		}
+		let curTime = Date.now();
+		lastChangeInterp = curTime;
+		// delay restarting the client by 3 seconds. i.e. if any other changes were made in 3
+		// seconds, then don't do the earlier change
+		setTimeout(async () => {
+			if (curTime !== lastChangeInterp) {
+				return;
+			}
+			// python interpreter changed. restart language client
+			if (client) {
+				client.stop();
+				client = null;
+			}
+			await startLSClient(context);
+		}, 3000);
+	})
+	const docSelector = {
+		language: 'antimony',
+		scheme: 'file',
+	}
+	let codeLensProviderDisposable = languages.registerCodeLensProvider(
+		docSelector,
+		new AntCodeLensProvider()
+	)
 
-	decorateDocument(window.activeTextEditor.document);
+	context.subscriptions.push(codeLensProviderDisposable)
 	workspace.onDidSaveTextDocument(decorateDocument);
-	workspace.onDidOpenTextDocument(decorateDocument);
-	window.onDidChangeActiveTextEditor(e => decorateDocument(e.document));
-	window.onDidChangeActiveColorTheme(() => decorateDocument(window.activeTextEditor.document));
+	workspace.onDidOpenTextDocument((doc: TextDocument) => {
+		decorateDocument(doc);
+	});
+	window.onDidChangeActiveTextEditor(async e => {
+		decorateDocument(e?.document)
+	});
+	window.onDidChangeActiveColorTheme(() => decorateDocument(window.activeTextEditor?.document));
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -68,6 +162,16 @@ export function deactivate(): Thenable<void> | undefined {
 async function createAnnotationDialog(context, args: any[]) {
 	// Wait till client is ready, or the Python server might not have started yet.
 	// NOTE this is necessary for any command that might use the Python language server.
+	if (!client) {
+		// TODO show error message
+		const choice = await window.showErrorMessage(
+			'Language server not running. Select a valid Python interpreter',
+			'Edit in settings');
+		if (choice === 'Edit in settings') {
+			await commands.executeCommand('workbench.action.openSettings', 'bio-ide.pythonInterpreter');
+		}
+		return;
+	}
 	await client.onReady();
 
 	const selection = window.activeTextEditor.selection
@@ -104,18 +208,21 @@ interface ServerRange {
 	end_line: number;
 	end_column: number;
 }
-const annotDecorTypeDark = window.createTextEditorDecorationType({	
+const annotDecorTypeDark = window.createTextEditorDecorationType({
 	borderStyle: 'none none solid none',
 	borderColor: 'white',
 	borderWidth: '1px',
 });
-const annotDecorTypeLight = window.createTextEditorDecorationType({	
+const annotDecorTypeLight = window.createTextEditorDecorationType({
 	borderStyle: 'none none solid none',
 	borderColor: 'black',
 	borderWidth: '1px',
 });
 
-async function decorateDocument(doc: TextDocument) {
+async function decorateDocument(doc: TextDocument | undefined) {
+	if (!doc || !client) {
+		return;
+	}
 	await client.onReady();
 	if (doc.languageId !== 'antimony') {
 		return;
@@ -123,13 +230,13 @@ async function decorateDocument(doc: TextDocument) {
 
 	let decorations: DecorationOptions[] = [];
 	const sranges: ServerRange[] = await commands.executeCommand('antimony.getAnnotated', doc.getText());
-    
+
 	for (const srange of sranges) {
 		const range = new Range(
 			new Position(srange.line, srange.column),
 			new Position(srange.end_line, srange.end_column),
 		);
-		
+
 		const decor = { range };
 		decorations.push(decor);
 	}
--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,4 +1,20 @@
+import * as cp from "child_process";
 
 export function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function execPromise(command: string) {
+    return new Promise(function (resolve, reject) {
+        cp.exec(command, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({
+                    stdout,
+                    stderr,
+                });
+            }
+        })
+    });
 }

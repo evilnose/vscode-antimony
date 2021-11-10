e468d7e3dee47f0c88d1c6c31dbc992f746f01e7
mastevb <bochenma@cs.washington.edu>
2021-11-10 01:39:48 -0800

    initializing repo

--- vscode-antimony/out/extension.js
+++ vscode-antimony/out/extension.js
@@ -16,7 +16,7 @@ function activate(context) {
     let disposable = vscode.commands.registerCommand('vscode-antimony.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from vscode-antimony!');
+        vscode.window.showInformationMessage('Hello World from vscode-antimony');
     });
     context.subscriptions.push(disposable);
 }

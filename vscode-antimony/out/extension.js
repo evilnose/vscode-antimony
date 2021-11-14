5f148e2d180d540bc83756b9c2413635d1d002f9
mastevb <bochenma@cs.washington.edu>
2021-11-13 16:37:15 -0800

    editing extensions.ts

--- vscode-antimony/out/extension.js
+++ vscode-antimony/out/extension.js
@@ -1,21 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 // For debugging
 let debug = vscode.window.createOutputChannel("Debug");
 debug.show();
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('antimony.createAnnotationDialog', (...args) => createAnnotationDialog(context, args)));
 }
 exports.activate = activate;
 async function createAnnotationDialog(context, args) {
+    debug.append("hi");
 }
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- vscode-antimony/src/extension.ts
+++ vscode-antimony/src/extension.ts
@@ -1,13 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
 
 // For debugging
 let debug = vscode.window.createOutputChannel("Debug");
 debug.show();
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('antimony.createAnnotationDialog',
@@ -18,5 +20,4 @@ async function createAnnotationDialog(context: vscode.ExtensionContext, args: an
 	
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}

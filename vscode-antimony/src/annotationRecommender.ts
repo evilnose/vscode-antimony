6f2200f20f970037b6a85972c028997eb201134d
evaliu2002 <liuxiner2002@gmail.com>
2022-09-27 16:42:14 -0700

    slight changes to obtaining dictionary from back end, doesn't work yet

---
+++ vscode-antimony/src/annotationRecommender.ts
@@ -0,0 +1,284 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Modified by Gary Geng and Steve Ma for the Antimony VSCode extension project.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { QuickPickItem, window, Disposable, QuickInputButton, QuickInput, ExtensionContext, QuickInputButtons, commands, QuickPick, ProgressLocation } from 'vscode';
+import { sleep } from './utils/utils';
+
+/**
+ * A multi-step input using window.createQuickPick() and window.createInputBox().
+ * 
+ * This first part uses the helper class `MultiStepInput` that wraps the API for the multi-step case.
+ */
+export async function singleStepInputRec(context: ExtensionContext, line: number, lineStr: string, charStr: string, uri: string, initialEntity: string = null) {
+    let databases = [];
+    let recommendations;
+	vscode.commands.executeCommand('antimony.recommender', lineStr, charStr, uri).then(async (result) => {
+        console.log(result)
+
+        recommendations = getResult(result);
+        console.log(recommendations[0])
+       
+        for (let i = 0; i < recommendations.length; i++) {
+            databases.push({id: recommendations[i]}); 
+        }
+
+        console.log(databases)
+
+    interface State {
+        title: string;
+        step: number;
+        totalSteps: number;
+        database: QuickPickItem;
+        entity: QuickPickItem;
+        initialEntity: string;
+    }
+
+    async function collectInputs() {
+        const state = {initialEntity} as Partial<State>;
+        await MultiStepInput.run(input => pickDatabase(input, state));
+        return state as State;
+    }
+
+    const title = 'Select Annotation';
+
+    async function pickDatabase(input: MultiStepInput, state: Partial<State>) {
+        const pick = await input.showQuickPick({
+            title,
+            step: 1,
+            totalSteps: 1,
+            placeholder: 'Select Annotation',
+            items: databases,
+            activeItem: state.database,
+            shouldResume: shouldResume,
+            onInputChanged: null,
+        });
+        state.database = pick;
+        onQueryUpdated(state.database['id'], input)
+    }
+
+    async function onQueryUpdated(annotation: string, input: MultiStepInput) {
+        const snippetStr = new vscode.SnippetString(annotation);
+        const doc = vscode.window.activeTextEditor.document;
+        const pos = doc.lineAt(line).range.end;
+        vscode.window.activeTextEditor.insertSnippet(snippetStr, pos);
+    }
+
+    function shouldResume() {
+        // Could show a notification with the option to resume.
+        return new Promise<boolean>((resolve, reject) => {
+            // noop
+        });
+    }
+
+    const state = await collectInputs();
+    return {
+        'database': state.database.label,
+        'entity': state.entity
+    }
+    // window.showInformationMessage(`Creating Application Service '${state.name}'`);
+    });
+}
+
+async function getResult(result) {
+	return result.annotations;
+}
+
+// -------------------------------------------------------
+// Helper code that wraps the API for the multi-step case.
+// -------------------------------------------------------
+
+
+class InputFlowAction {
+    static back = new InputFlowAction();
+    static cancel = new InputFlowAction();
+    static resume = new InputFlowAction();
+}
+
+type InputStep = (input: MultiStepInput) => Thenable<InputStep | void>;
+
+interface QuickPickParameters<T extends QuickPickItem> {
+    title: string;
+    step: number;
+    totalSteps: number;
+    items: T[];
+    activeItem?: T;
+    placeholder: string;
+    buttons?: QuickInputButton[];
+    initialValue?: string;
+    shouldResume: () => Thenable<boolean>;
+    onInputChanged: (v: string) => void;
+}
+
+interface InputBoxParameters {
+    title: string;
+    step: number;
+    totalSteps: number;
+    value: string;
+    prompt: string;
+    validate: (value: string) => Promise<string | undefined>;
+    buttons?: QuickInputButton[];
+    shouldResume: () => Thenable<boolean>;
+}
+
+export class MultiStepInput {
+
+    static async run<T>(start: InputStep) {
+        const input = new MultiStepInput();
+        return input.stepThrough(start);
+    }
+
+    current?: QuickInput;
+    private steps: InputStep[] = [];
+    private lastErrorMillis = 0;
+
+    private async stepThrough<T>(start: InputStep) {
+        let step: InputStep | void = start;
+        while (step) {
+            this.steps.push(step);
+            if (this.current) {
+                this.current.enabled = false;
+                this.current.busy = true;
+            }
+            try {
+                step = await step(this);
+            } catch (err) {
+                if (err === InputFlowAction.back) {
+                    this.steps.pop();
+                    step = this.steps.pop();
+                } else if (err === InputFlowAction.resume) {
+                    step = this.steps.pop();
+                } else if (err === InputFlowAction.cancel) {
+                    step = undefined;
+                } else {
+                    throw err;
+                }
+            }
+        }
+        if (this.current) {
+            this.current.dispose();
+        }
+    }
+
+    async showQuickPick<T extends QuickPickItem, P extends QuickPickParameters<T>>(
+        { title, step, totalSteps, items, activeItem, placeholder, buttons, initialValue, shouldResume, onInputChanged }: P) {
+        const disposables: Disposable[] = [];
+        try {
+            return await new Promise<T | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
+                const input = window.createQuickPick<T>();
+                input.title = title;
+                input.step = step;
+                input.totalSteps = totalSteps;
+                input.placeholder = placeholder;
+                input.items = items;
+                if (initialValue) {
+                    input.value = initialValue;
+                    onInputChanged(initialValue);
+                }
+                if (activeItem) {
+                    input.activeItems = [activeItem];
+                }
+                input.buttons = [
+                    ...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
+                    ...(buttons || [])
+                ];
+                disposables.push(
+                    input.onDidTriggerButton(item => {
+                        if (item === QuickInputButtons.Back) {
+                            reject(InputFlowAction.back);
+                        } else {
+                            resolve(<any>item);
+                        }
+                    }),
+                    input.onDidChangeSelection(items => resolve(items[0])),
+                    input.onDidHide(() => {
+                        (async () => {
+                            reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
+                        })()
+                            .catch(reject);
+                    }),
+                    ...(onInputChanged ? [input.onDidChangeValue(onInputChanged)] : []),
+                );
+                if (this.current) {
+                    this.current.dispose();
+                }
+                this.current = input;
+                this.current.show();
+            });
+        } finally {
+            disposables.forEach(d => d.dispose());
+        }
+    }
+
+    instanceOfQuickPick(input): input is QuickPick<QuickPickItem> {
+        return 'items' in input;
+    }
+
+    async onQueryResults(result) {
+        if (this.current && this.current.step === 2) {
+            return result;
+        }
+    }
+
+    async showInputBox<P extends InputBoxParameters>({ title, step, totalSteps, value, prompt, validate, buttons, shouldResume }: P) {
+        const disposables: Disposable[] = [];
+        try {
+            return await new Promise<string | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
+                const input = window.createInputBox();
+                input.title = title;
+                input.step = step;
+                input.totalSteps = totalSteps;
+                input.value = value || '';
+                input.prompt = prompt;
+                input.buttons = [
+                    ...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
+                    ...(buttons || [])
+                ];
+                let validating = validate('');
+                disposables.push(
+                    input.onDidTriggerButton(item => {
+                        if (item === QuickInputButtons.Back) {
+                            reject(InputFlowAction.back);
+                        } else {
+                            resolve(<any>item);
+                        }
+                    }),
+                    input.onDidAccept(async () => {
+                        const value = input.value;
+                        input.enabled = false;
+                        input.busy = true;
+                        if (!(await validate(value))) {
+                            resolve(value);
+                        }
+                        input.enabled = true;
+                        input.busy = false;
+                    }),
+                    input.onDidChangeValue(async text => {
+                        const current = validate(text);
+                        validating = current;
+                        const validationMessage = await current;
+                        if (current === validating) {
+                            input.validationMessage = validationMessage;
+                        }
+                    }),
+                    input.onDidHide(() => {
+                        (async () => {
+                            reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
+                        })()
+                            .catch(reject);
+                    })
+                );
+                if (this.current) {
+                    this.current.dispose();
+                }
+                this.current = input;
+                this.current.show();
+            });
+        } finally {
+            disposables.forEach(d => d.dispose());
+        }
+    }
+}

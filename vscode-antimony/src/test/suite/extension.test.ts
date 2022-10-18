994bc536e33dd9450d4d4e7af8af490fe65b166a
Eva Liu <liuxiner2002@gmail.com>
2022-10-18 14:15:31 -0700

    Revert "Some progress for front end unit testing"
    This reverts commit ab31baaac589bde65dacc8d2cb3db6c8b3b885b8.

--- vscode-antimony/src/test/suite/extension.test.ts
+++ vscode-antimony/src/test/suite/extension.test.ts
@@ -8,19 +8,8 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	// Testing VSCode Response of Rate Law Button in Context Menu
-	test("Testing VSCode Rate Law Context Menu Button Response", async () => {
-        await vscode.commands.executeCommand('antimony.insertRateLawDialog');
-        if (console.error() == null){
-            assert(true);
-        } else {
-            assert(false);
-        }
-    });
-
-	// 
-	// 
-
-
-
+	test('Sample test', () => {
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
 });

620c6b3c1e8e0a87c5242a8717337e48d2277f8a
Eva Liu <evaxinerliu@gmail.com>
2023-01-25 17:20:43 -0800

    linux virtual env download

--- vscode-antimony/src/runshell.ts
+++ vscode-antimony/src/runshell.ts
@@ -6,12 +6,15 @@ import * as path from 'path';
 var shell = require('shelljs');
 shell.echo ("script runs");
 var current_path_to_silicon_shell = path.join(__dirname, '..', 'src', 'server', 'virtualEnvSilicon.sh');
+var current_path_to_linux_shell = path.join(__dirname, '..', 'src', 'server', 'virtualEnvLinux.sh');
 
-if (os.platform().toString() == 'darwin' || os.platform().toString() == 'linux') {
+if (os.platform().toString() == 'darwin') {
     shell.exec('sh ' + current_path_to_silicon_shell)
 } else if (os.platform().toString() == 'win32') {
     var path_to_win_shell = path.join(__dirname, 'server');
     var path_to_user_folder = path.join(__dirname, '..', '..')
     shell.cd(path_to_win_shell)
     shell.exec(path_to_win_shell + '\\virtualEnvWin.bat')
+} else if (os.platform().toString() == 'linux') {
+    shell.exec('sh ' + current_path_to_linux_shell)
 }
